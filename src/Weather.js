ca1e1a122d9c190acb75ad7cb0461fe13f7bb187
SeattleDantz <tricia@curo-software.com>
2021-04-09 22:48:48 -0700

    Added axios to check Seattle weather and a loader

---
+++ src/Weather.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    setTemperature(Math.round(response.data.main.temp));
+  }
+  const [temperature, setTemperature] = useState("");
+  let apiKey = "551f8c89cdce818fb4f3b6e3fe374a5c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <div className="Weather">
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
+      <h2>
+        The weather in {props.city} is {temperature}℉.
+      </h2>
+    </div>
+  );
+}
